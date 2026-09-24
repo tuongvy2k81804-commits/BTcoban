ee5e883da0faca6e070af29b5033a253b84e6fe3
NGO TUONG VY <tuongvy2k81804@gmail.com>
2026-09-25 02:11:42 +0700

    Add C++ program to calculate factorial of n

---
+++ vòng lặp /vòng lặp cơ bản /tính giai thừa n!.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std;
+
+int main () {
+	int n; cin >> n;
+	
+	int giaithua = 1;
+	
+	for ( int i = 1; i <= n; i ++) {
+		giaithua *= i;
+	}
+	cout << giaithua;
+}
