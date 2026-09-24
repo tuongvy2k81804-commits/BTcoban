aa60ff136e02f6d299e9d9abb710542b2c668299
NGO TUONG VY <tuongvy2k81804@gmail.com>
2026-09-25 01:29:27 +0700

    Update tính giai thừa n! .cpp

--- vòng lặp cơ bản /tính giai thừa n! .cpp
+++ vòng lặp cơ bản /tính giai thừa n! .cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main () {
+	int n; cin >> n;
+	
+	int giaithua = 1;
+	
+	for ( int i = 1; i <= n; i++) {
+		giaithua *= i;
+	}
+	cout << giaithua;
+	
+	return 0;
+}
