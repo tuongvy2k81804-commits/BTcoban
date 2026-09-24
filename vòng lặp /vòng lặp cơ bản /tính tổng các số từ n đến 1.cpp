e801f9580cfebc6ce10a1fb09801b725530cd018
NGO TUONG VY <tuongvy2k81804@gmail.com>
2026-09-25 01:55:04 +0700

    Create tính tổng các số từ n đến 1.cpp

---
+++ vòng lặp /vòng lặp cơ bản /tính tổng các số từ n đến 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int n; cin >> n;
+	
+	int tong = 0;
+	
+	for (int i = 1; i <= n; i ++) {
+		tong += i;
+	}
+	cout << tong;
+	
+	return 0;
+}
