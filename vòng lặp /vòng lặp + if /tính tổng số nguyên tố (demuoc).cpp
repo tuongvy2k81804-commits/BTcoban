62a06eb750a6d760bb4c9bf192a6e4e1fbabd4af
NGO TUONG VY <tuongvy2k81804@gmail.com>
2026-09-26 23:59:01 +0700

    Update tính tổng số nguyên tố (demuoc).cpp

--- vòng lặp /vòng lặp + if /tính tổng số nguyên tố (demuoc).cpp
+++ vòng lặp /vòng lặp + if /tính tổng số nguyên tố (demuoc).cpp
@@ -7,15 +7,15 @@ int main () {
 	
 	int tong = 0;
 	
-	for ( int i = 2; i <= n; i++) {
+	for ( int i = 2; i <= n; i++) {      // xét từ i = 2
 		int dem = 0;
 	
-		for ( int j = 2; j <= i - 1; j++) {
+		for ( int j = 2; j <= i - 1; j++) {        // j <= i - 1 ( lúc này n là n thứ i)
 			if ( i % j == 0) {
 				dem ++;
 			}
 	}
-		if ( dem == 0) {
+		if ( dem == 0) {          //tổng đặt ngoài for j, vì đặt trong j, mỗi lần j tăng, đem lại về 0
 			tong += i;
 		}
 	}
